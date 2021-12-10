9e1597487bc68a6cac427ab2bedb69143f3c0ea7
goodsvpg <goodbeprose26@gmail.com>
2021-12-10 19:10:00 +0900

    fix(상수 치환)

--- src/main/java/stringcaculator/domain/Delimiter.java
+++ src/main/java/stringcaculator/domain/Delimiter.java
@@ -4,6 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Delimiter {
+    private static final Pattern CUSTOM_REGEX = Pattern.compile(DelimiterRegex.CUSTOM.toString());
+    private static final int CUSTOM_DELIMITER = 1;
+    private static final int REST_INPUT = 2;
     private final Input input;
 
     private Matcher matcher;
@@ -13,13 +16,13 @@ public class Delimiter {
     }
 
     public boolean useCustomDelimeter() {
-        this.matcher = Pattern.compile(DelimiterRegex.CUSTOM.toString()).matcher(input.getValue());
+        this.matcher = CUSTOM_REGEX.matcher(input.getValue());
         return matcher.find();
     }
 
     public String[] splitedInput(){
         if (useCustomDelimeter()) {
-            return this.matcher.group(2).split(matcher.group(1));
+            return this.matcher.group(REST_INPUT).split(matcher.group(CUSTOM_DELIMITER));
         }
         return input.splitedInput(DelimiterRegex.GENERAL);
     }
